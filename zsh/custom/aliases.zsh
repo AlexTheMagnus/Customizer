58d84a259230397cd057b8f5364a5168285a2e72
AlexTheMagnus <i72lulem@uco.es>
2020-07-31 18:52:56 +0200

    Updated aliases.zsh

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -3,16 +3,15 @@ alias pdf='xdg-open'
 alias mkae='make'
 
 # META ALIASES
-alias aliases="gedit $HOME/dotfiles/zsh/custom/aliases.zsh"
-alias dotfiles-update="source $HOME/dotfiles/update.sh"
+alias aliases="gedit $HOME/Customizer/zsh/custom/aliases.zsh"
 alias project_hex='cd /opt/Project_Hex && sudo ./Project_Hex.out && cd -'
 alias system-upgrade='sudo apt-get upgrade && project_hex'
 
 # DIRECTORIES
-alias screenshots='nautilus /home/alex/Pictures/screenshots &'
+alias screenshots="nautilus $HOME/Pictures/screenshots &"
 
 # TEMPLATES
-alias templatex="cp $HOME/dotfiles/templates/latexTemplate.tex . && mv latexTemplate.tex"
+alias templatex="cp $HOME/Customizer/templates/latexTemplate.tex $(pwd)"
 
 # OTHERS
 alias ucossh="ssh -X i72lulem@ts.uco.es"
