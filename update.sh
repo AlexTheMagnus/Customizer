fc359b4949f5e0c7a9451a1262046ef072ba94f7
AlexTheMagnus <i72lulem@uco.es>
2020-07-31 18:59:25 +0200

    Added folder verification

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 # CREATE OR UPDATE SOFT LINKS
 ln -sfn $HOME/Customizer/zsh/.zshrc $HOME/.zshrc
 ln -sfn $HOME/Customizer/git/.gitconfig $HOME/.gitconfig
-ln -sfn $HOME/Customizer/code/settings.json $HOME/.config/Code/User/settings.json
+mkdir -p $HOME/.config/Code/User/ && ln -sfn $HOME/Customizer/code/settings.json $HOME/.config/Code/User/settings.json
 
 # RELOAD SHELL CONFIGURATION
 source $HOME/.zshrc
